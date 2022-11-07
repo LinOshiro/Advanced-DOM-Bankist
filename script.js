0d4ff8761fe14888fc5ef34657cbd1c7641e8705
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-11-07 13:44:38 -0600

    Building tab component

--- script.js
+++ script.js
@@ -74,3 +74,74 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Tabbed Component:
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard Clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+///////////////////////////////////
+///////////////////////////////////
+///////////////////////////////////
+
+//html: operations, btn operations tab, content
+
+/////////////////////////////////////////////////////////////////////
+// NOTES:
+/*
+const h1 = document.querySelector('h1');
+
+// Going downwards: child elements (.querySelector)
+console.log(h1.querySelectorAll('.highlight'));
+//NodeList(2) [span.highlight, span.highlight]
+console.log(h1.childNodes);
+//NodeList(9) [text, comment, text, span.highlight, text, br, text, span.highlight, text]
+console.log(h1.children);
+//HTMLCollection(3) [span.highlight, br, span.highlight]
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: Parents
+console.log(h1.parentNode);
+// header__title
+console.log(h1.parentElement);
+// header__title
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going Sideways: siblings
+console.log(h1.previousElementSibling); // null
+console.log(h1.nextElementSibling); //<h4>Simpler banking experience for a simpler life</h4>
+
+console.log(h1.previousSibling); // #text
+console.log(h1.nextSibling); // #text
+
+console.log(h1.parentElement.children); // Shows all the siblings
+// HTMLCollection(4) [h1, h4, button.btn--text.btn--scroll-to, img.header__img]
+
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+// Makes all the others 50% smaller
+*/
